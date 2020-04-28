ea979bc2bc8500ebe4a5adae25e7fbc7efb24eab
Mike Ossig <mike.ossig@adyen.com>
2020-04-28 11:31:50 -0400

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, withRouter } from 'react-router-dom';
+import Header from './Header';
+import { OptionsForm } from './OptionsForm';
+
+const App = () => {
+
+  return (
+    <div id="app">
+      <Header />
+      <Route exact path to='/' component={OptionsForm} />
+    </div>
+  )
+};
+
+export default withRouter(App)
---
+++ src/components/adyen/component/ComponentForm.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import {
+  Form,
+  Input,
+  Label,
+  Button,
+  FormText,
+  FormGroup
+} from 'reactstrap';
+
+export const ComponentForm = props => {
+  return (
+    <FormGroup>
+      
+    </FormGroup>
+  );
+};
